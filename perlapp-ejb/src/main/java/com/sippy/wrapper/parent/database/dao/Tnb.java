d8ca5e03d68e638f1490a88a831618ee43a1f221
Larissa Ferme <ferme@sipgate.de>
2024-11-28 15:20:24 +0100

    add files

---
+++ perlapp-ejb/src/main/java/com/sippy/wrapper/parent/database/dao/Tnb.java
@@ -0,0 +1,10 @@
+package com.sippy.wrapper.parent.database.dao;
+
+public class Tnb extends TnbDao{
+
+    boolean isTnb;
+
+    public boolean getIsTnb(boolean isTnb) {return isTnb;}
+
+    public void setIsTnb(boolean isTnb) {this.isTnb = isTnb;}
+}
---
+++ perlapp-ejb/src/main/java/com/sippy/wrapper/parent/request/Params.java
@@ -0,0 +1,13 @@
+package com.sippy.wrapper.parent.request;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Params {
+
+    String number;
+
+    public String getNumber(){return number;}
+
+}
